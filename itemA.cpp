5d3c2c6042038838b8ccac08a5369dcbb5d0a7e0
jean <jeansousa2001@gmail.com>
2023-10-09 19:01:54 -0300

    Implementação do item C realizada e testada a partir dos dados do item D.

--- itemA.cpp
+++ itemA.cpp
@@ -1,18 +1,35 @@
 #include <iostream>
 #include <cmath>
 #include <cstdlib>
+#include <iomanip>
 
 using namespace std;
+
 
 double newton_itemA(double a3, double a2, double d0, double lambda, double epsilon);
 double derivada_f(double d, double a3, double a2);
 double funcao_d(double d, double a3, double a2); 
 double newton_itemB(double a3, double a2, double d0, double lambda, double epsilon);
+double derivada_diferenca_finita(double d, double a3, double a2, double (*func)(double, double, double), double h);
+double newton_itemC(double a3, double a2, double d0, double lambda, double epsilon, double  h);
+
 
 int main(){
+    std::cout << std::fixed << std::setprecision(15);
 
-    double raiz = newton_itemB(1, 1, 1.5, 0.05, 0.001);
-    cout << raiz << endl;
+    // Item A - Cálculo da raiz de f(d) = a3d3 – 9a2d + 3 por Newton-Raphson normal
+    double raizA = newton_itemA(1, 1, 0.5, 0.05, 0.001);
+    cout << "A) Raiz: " << raizA << endl;
+
+    // Item B - Cálculo da raiz de f(d) = a3d3 – 9a2d + 3 por Newton-Raphson modificado
+    cout << "B) ";
+    double raizB = newton_itemB(1, 1, 0.5, 0.05, 0.001);
+    cout << raizB << endl;
+
+    // Item C - Cálculo da raiz de f(d) = a3d3 – 9a2d + 3 por Newton-Raphson
+    // com derivada calculada pelo método da diferença finita
+    double raizC = newton_itemC(1, 1, 0.5, 0.05, 0.001, 0.0001);
+    cout << "C) Raiz: " << raizC << endl;
 
 }
 
@@ -52,7 +69,7 @@ double newton_itemB(double a3, double a2, double d0, double lambda, double epsil
 
         double d1 = d0 - (funcao_d(d0, a3, a2)/derivada_f(d0, a3, a2));  
         double derivada = derivada_f(d0, a3, a2);
-        while(abs(funcao_d(d0, a3, a2) >= epsilon || abs(d1-d0) >= epsilon)){    
+        while(abs(funcao_d(d0, a3, a2)) >= epsilon || abs(d1-d0) >= epsilon){
             d0 = d1;
             cout << derivada << endl;
         
@@ -68,8 +85,27 @@ double newton_itemB(double a3, double a2, double d0, double lambda, double epsil
         return d1;
     }
     
-    cout << "nao deu bom" << endl;
-    return 0.0;
+    cout << "Erro! f'(d0) < lambda: ";
+    return derivada_f(d0, a3, a2);
+}
 
+// Calculo da derivada pelo método da diferença finita
+double derivada_diferenca_finita(double d, double a3, double a2, double (*func)(double, double, double), double h){
+    return (func(d + h, a3, a2) - func(d, a3, a2)) / h;
+}
+
+// Implementação do Item C
+double newton_itemC(double a3, double a2, double d0, double lambda, double epsilon, double h){
+    if (abs(funcao_d(d0, a3, a2)) < epsilon){
+        return d0;
+    }
+
+    double d1 = d0 - (funcao_d(d0, a3, a2)/derivada_diferenca_finita(d0, a3, a2, funcao_d, h));
+    while(abs(funcao_d(d0, a3, a2)) >= epsilon || abs(d1-d0) >= epsilon){
+        d0 = d1;
+        d1 = d0 - (funcao_d(d0, a3, a2)/derivada_diferenca_finita(d0, a3, a2, funcao_d, h));
+    }
+
+    return d1;
 
 }
