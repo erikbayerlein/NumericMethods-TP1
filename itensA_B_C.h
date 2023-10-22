0065e4e51765f12846f48fd6e7fe79e7be6bb405
erikbayerlein <erik.bayer@icloud.com>
2023-10-22 10:19:15 -0300

    menu.cpp changed to work with itens_A_B_C.h

--- itensA_B_C.h
+++ itensA_B_C.h
@@ -2,19 +2,19 @@
 #define itensA_B_C_H
 
 
-static void desenhar_tabela(int k, float d, float fd);
+void desenhar_tabela(int k, float d, float fd);
 
-static double newton_itemA(double a3, double a2, double d0, double epsilon);
+double newton_itemA(double a3, double a2, double d0, double epsilon);
 
-static double derivada_f(double d, double a3, double a2);
+double derivada_f(double d, double a3, double a2);
 
-static double funcao_d(double d, double a3, double a2);
+double funcao_d(double d, double a3, double a2);
 
-static double newton_itemB(double a3, double a2, double d0, double lambda, double epsilon);
+double newton_itemB(double a3, double a2, double d0, double lambda, double epsilon);
 
-static double derivada_diferenca_finita(double d, double a3, double a2, double (*func)(double, double, double), double h);
+double derivada_diferenca_finita(double d, double a3, double a2, double (*func)(double, double, double), double h);
 
-static double newton_itemC(double a3, double a2, double d0, double lambda, double epsilon, double h);
+double newton_itemC(double a3, double a2, double d0, double lambda, double epsilon, double h);
 
 
 #endif //itensA_B_C_H
